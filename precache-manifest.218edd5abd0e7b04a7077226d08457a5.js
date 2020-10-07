c906ae89becdea601f0822e460f081a683ccf7c2
wangqiushuang <485969746@qq.com>
2020-10-08 00:01:09 +0800

    update

---
+++ precache-manifest.218edd5abd0e7b04a7077226d08457a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e666affc562b40ca27",
+    "url": "/xiaowangaccount/css/app.8d88636f.css"
+  },
+  {
+    "revision": "88284d6d5af82a0e5754a054881d0e08",
+    "url": "/xiaowangaccount/index.html"
+  },
+  {
+    "revision": "69e666affc562b40ca27",
+    "url": "/xiaowangaccount/js/app.a29e62ab.js"
+  },
+  {
+    "revision": "4fc21417c8bb842193bd",
+    "url": "/xiaowangaccount/js/chunk-vendors.00d2efb6.js"
+  },
+  {
+    "revision": "5a3bc337ed9627c080692ef3c952b5ba",
+    "url": "/xiaowangaccount/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/xiaowangaccount/robots.txt"
+  }
+]);
